478b6b6512ab10ce90e60479bc6668bc2d31410a
Kevin Hu <67344175+kevinhu12@users.noreply.github.com>
2023-12-19 21:58:53 -0500

    changed font to LeagueSpartan

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -13,7 +13,7 @@ const AboutMe = ({
         <Section>
             <Grid container spacing={theme.gridSpacing}>
                 <Grid item xs={6}>
-                    <Typography color='primary'>text</Typography>
+                    <Typography color='primary' variant='h4' sx={{ fontWeight: 'bold' }}>About Me</Typography>
                     <Typography color='primary'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Typography>
                 </Grid>
                 <Grid item xs={6}>
--- src/theme.js
+++ src/theme.js
@@ -1,6 +1,9 @@
 import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
+    typography: {
+        fontFamily: ['League Spartan', 'regular'].join(',')
+    },
     palette: {
         primary: { main: '#000000' },
         secondary: { main: '#ffffff' }
